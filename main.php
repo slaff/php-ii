e103c96a03467ea49ed9843ecf01399bbaa52a63
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-12 23:49:14 -0800

    Added area calculation and multiplication using adddition.

--- main.php
+++ main.php
@@ -6,3 +6,9 @@ use Libs\Calc;
 $calc = new Calc();
 $calc->plus(2,3);
 echo $calc->getMemory();
+
+
+// pizza 20cm diameter
+echo $calc->areaCircle(10);
+echo "\n";
+echo $calc->multiply(3,4);
--- src/Calc.php
+++ src/Calc.php
@@ -3,6 +3,8 @@ namespace Libs;
 
 class Calc 
 {
+  const PI = 3.14; // constant
+
   private $memory;
 
   public function plus($a, $b) {
@@ -15,8 +17,23 @@ class Calc
     return $this->memory;
   }
 
+  public function multiply($a, $b) {
+     // 3 * 4 = 3 + 3 + 3 + 3
+     $result = 0; 
+     for($i=0; $i< $b; $i++) {
+         $result = $this->plus($result, $a); // call another method from the same class
+     }
+     
+     return $result;
+  }
 
   public function getMemory() {
-    return $this->memory;
+    return $this->memory; // call property from the same class
+  }
+
+  public function areaCircle($radius) {
+    // π * pow(r, 2)  
+    $result = self::PI * pow($radius, 2); // call constant from the same class
+    return $result;
   }
 } 
