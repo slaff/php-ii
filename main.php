4bd39c0770f767b0de726552539576db985f29d3
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-14 14:14:25 +0200

    Preparation for the test.

--- main.php
+++ main.php
@@ -5,17 +5,6 @@ require __DIR__.'/vendor/autoload.php';
 
 use Libs\{User, Db\Repository };
 
-$url = "https://maps.googleapis.com/maps/api/geocode/json?address=Larnaca,Cyprus";
-
-$json = file_get_contents($url);
-
-$data = json_decode($json, true);
-
-var_dump($data);
-
-exit;
-
-
 // 1. using ORM => you get an object which is instance of the User class
 
 $config = include __DIR__.'/config/application.config.php';
